eabc87c36124811ae51048fd38cfafcbd1d0d448
YATHARTH VARDAN <yatharthvardan@gmail.com>
2020-07-13 21:06:44 +0530

    l

--- routes/hereMap.js
+++ routes/hereMap.js
@@ -5,20 +5,20 @@ const request = require('request');
 
 Router.use(bodyParser.json());
 
-// Router.route('/sP/:place')
-// .get((req,res,next) => {
-//   res.set('Access-Control-Allow-Origin', 'https://yatharthvardan.github.io');
-//   res.set('Access-Control-Allow-Credentials','true');
-//     request(
-//         { url: 'https://geocode.search.hereapi.com/v1/geocode?q=' + req.params.place + '&apiKey=Yd-fnbk9FQ9yAsp35VV5rXMlCnMVJTS4eBk2f3wIkns'},
-//         (error, response, body) => {
-//           if (error || response.statusCode !== 200) {
-//             return res.status(500).json({ type: 'error', message: err.message });
-//           }
-//           res.json(JSON.parse(body));
-//         }
-//       )
-// });
+Router.route('/sP/:place')
+.get((req,res,next) => {
+  res.set('Access-Control-Allow-Origin', 'https://yatharthvardan.github.io');
+  res.set('Access-Control-Allow-Credentials','true');
+    request(
+        { url: 'https://geocode.search.hereapi.com/v1/geocode?q=' + req.params.place + '&apiKey=Yd-fnbk9FQ9yAsp35VV5rXMlCnMVJTS4eBk2f3wIkns'},
+        (error, response, body) => {
+          if (error || response.statusCode !== 200) {
+            return res.status(500).json({ type: 'error', message: err.message });
+          }
+          res.json(JSON.parse(body));
+        }
+      )
+});
 
 Router.route('/sC/:category')
 .get((req,res,next) => {
