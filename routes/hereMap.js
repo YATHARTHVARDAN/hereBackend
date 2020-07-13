018d68968affe220896017a7e1c4952eeae5efda
YATHARTH VARDAN <yatharthvardan@gmail.com>
2020-07-13 21:11:27 +0530

    l

--- routes/hereMap.js
+++ routes/hereMap.js
@@ -20,24 +20,4 @@ Router.route('/sP/:place')
       )
 });
 
-Router.route('/sC/:category')
-.get((req,res,next) => {
-  res.set('Access-Control-Allow-Origin', 'https://yatharthvardan.github.io');
-  res.set('Access-Control-Allow-Credentials','true');
-  request(
-    {
-      url: 'https://discover.search.hereapi.com/v1/discover/?in=circle:' + req.lat + ','+req.lng+';r='+req.radius+'&q='+req.params.category+'&apiKey=Yd-fnbk9FQ9yAsp35VV5rXMlCnMVJTS4eBk2f3wIkns'
-    },
-    (err,response,body) => {
-      if(err || response.statusCode!==200)
-      {
-        return res.status(response.statusCode).json({type:'error',message:err.message});
-      }
-      res.statusCode = 200;
-      res.setHeader('Content-Type','application/json');
-      res.json(JSON.parse(body));
-    }
-  )
-});
-
 module.exports = Router;
